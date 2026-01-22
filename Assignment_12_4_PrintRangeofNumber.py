1ac877f8a8c3fd4829d5d166677c383b3e790db4
shrilekhabamane47-blip <shrilekhabamane47@gmail.com>
2026-01-22 21:19:40 +0530

    Delete Assignment_12_4_PrintRangeofNumber.py

--- Assignment_12_4_PrintRangeofNumber.py
+++
@@ -1,9 +0,0 @@
-#Write a program which accepts one number and prints that many numbers starting from 1
-def PrintRangeOfNumbers(num):
-    for i in range(1, num + 1):
-        print(i, end=" ")
-def main():
-    No = int(input("Enter a Number : "))
-    PrintRangeOfNumbers(No)
-if __name__ == "__main__":
-    main()
