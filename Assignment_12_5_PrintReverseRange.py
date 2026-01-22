307261406921204ea2fe9c7613d14bc756ae3ede
shrilekhabamane47-blip <shrilekhabamane47@gmail.com>
2026-01-22 21:19:28 +0530

    Delete Assignment_12_5_PrintReverseRange.py

--- Assignment_12_5_PrintReverseRange.py
+++
@@ -1,9 +0,0 @@
-#Write a program which accepts one number and prints that many numbers in reverse order
-def PrintReverseRange(num):
-    for i in range(num, 0, -1):
-        print(i, end=" ")
-def main():
-    No = int(input("Enter a Number : "))
-    PrintReverseRange(No)   
-if __name__ == "__main__":
-    main()
